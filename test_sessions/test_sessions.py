e3941b2fbd5cc309df6b39dbae7a4500b61273f1
ehean <eheaney98@gmail.com>
2017-10-11 19:38:42 -0400

    updated test_sessions so that it simulates a basic login/logout functionality

--- test_sessions/test_sessions.py
+++ test_sessions/test_sessions.py
@@ -1,6 +1,8 @@
 # testing webapp2 sessions
 import webapp2
 import session_handler
+import json
+from google.appengine.ext.webapp import template
 
 config = {}
 config['webapp2_extras.sessions'] = {
@@ -9,21 +11,44 @@ config['webapp2_extras.sessions'] = {
 
 
 # [START main_page]
-class LoginPage(webapp2.RequestHandler):
+class LoginPage(session_handler.BaseHandler):
 	def get(self):
-		self.response.write('login page')
+		self.response.write(template.render('login.html', {}))
+		
+	def post(self):
+		username = self.request.get('username')	
+		password = self.request.get('password')
+		# login_data = json.loads(self.request.body)
+		if username == "admin" and password == "password":
+			self.session['user'] = "admin"
+			self.redirect('/home')
+		else:
+			invalid_creds_template = {
+				'message': 'Your username and/or password is invalid. Please try again or contact an administrator.'
+			}
+			self.response.write(template.render('login.html', invalid_creds_template))
+	
 # [END main_page]
 
 class HomePage(session_handler.BaseHandler):
 	def get(self):
-		self.session['user'] = "test"
 		session = self.session.get('user')
 		if session == None:
 			self.redirect('/login')
 		else:
-			self.response.write("home page")
-			self.response.write("\n\n")
-			self.response.write("the user session key is: " + str(session))
+			home_page_template = {
+				'name': session
+			}
+			self.response.write(template.render('home.html', home_page_template))
+			
+	def post(self):
+		logout = self.request.get('logout')
+		
+		if logout == 'Logout':
+			self.redirect('/login')
+			self.session.clear()
+			
+		
 # [END main_page]
 
 
