8e6f01b42d5cb458e86db5a938dffd79e6807793
ehean <eheaney98@gmail.com>
2017-10-17 19:40:54 -0400

    Added a simple award creation page on user portal. Sends email and stores award in database upon creation.

--- sessions_test/create_entities.py
+++ sessions_test/create_entities.py
@@ -22,8 +22,8 @@ class Account(ndb.Model):
 	password = ndb.StringProperty(required=True)
 	name = ndb.StringProperty()
 	account_type = ndb.StringProperty()
-	creation_date = ndb.DateTimeProperty()
-	last_modified = ndb.DateTimeProperty()
+	creation_date = ndb.DateTimeProperty(auto_now_add=True )
+	last_modified = ndb.DateTimeProperty(auto_now=True)
 	signature = ndb.BlobProperty()
 
 	
@@ -33,7 +33,7 @@ class Award(ndb.Model):
 	recipient_email = ndb.StringProperty(required=True)
 	recipient_name = ndb.StringProperty()
 	award_type = ndb.StringProperty()
-	date_sent = ndb.DateTimeProperty()
+	date_sent = ndb.DateTimeProperty(auto_now_add=True)
 	# region = ndb.GeoPtProperty()
 
 
@@ -56,6 +56,19 @@ class AccountCollectionHandler(session_handler.BaseHandler):
 			self.response.write(json.dumps(account, cls=MyEncoder))
 			self.response.write('\n')
 			
+			
+			
+class AwardCollectionHandler(session_handler.BaseHandler):
+	
+	def get(self):
+		query = Award.query()
+		for award in query:
+			award = award.to_dict()
+			self.response.write(json.dumps(award, cls=MyEncoder))
+			self.response.write('\n')
+			
+			
+			
 # This is the AccountHandler. It handles the post, patch, get
 # and delete functions. It returns the JSON of the entity upon 
 # success, or an error message if the ID is not found.
@@ -76,9 +89,7 @@ class AccountHandler(session_handler.BaseHandler):
 		else:
 			new_account = Account(username=account_data["username"],
 			password=account_data["password"],
-			account_type=account_data['account_type'],
-			creation_date=datetime.datetime.now(), 
-			last_modified=datetime.datetime.now())
+			account_type=account_data['account_type'])
 			
 			if 'name' in account_data:
 				new_account.name = account_data['name']
@@ -110,9 +121,7 @@ class AccountHandler(session_handler.BaseHandler):
 					account.account_type = account_data['account_type']
 				if 'signature' in account_data:
 					account.signature = account_data['signature']
-				
-				account.last_modified = datetime.datetime.now()
-				
+								
 				account.put()
 				return json.dumps(account.to_dict())
 				# self.response.write(json.dumps(account.to_dict()))
@@ -153,15 +162,17 @@ class AccountHandler(session_handler.BaseHandler):
 # success, or an error message if the ID is not found.
 class AwardHandler(session_handler.BaseHandler):
 
-	def post(self):
+	def post(self, body):
 		
-		award_data = json.loads(self.request.body)
+		if body:
+			award_data = body
+		else:
+			award_data = json.loads(self.request.body)
 		
 		new_award = Award( \
 		sender=award_data["sender"], \
 		recipient_name=award_data["recipient_name"], \
 		recipient_email=award_data["recipient_email"], \
-		date_sent=datetime.datetime.now(), \
 		award_type=award_data["award_type"]) 
 
 		new_award.put()
@@ -169,7 +180,7 @@ class AwardHandler(session_handler.BaseHandler):
 		new_award.put()
 		
 		# self.response.write(json.dumps(award.to_dict()))
-		return json.dumps(award.to_dict())
+		return True
 	
 	# User should not be able to edit an award once its sent.
 	
