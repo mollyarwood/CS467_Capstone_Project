e5b471867ebbe7181f1a9985d63b87299613ebd1
Matt Fry <fryma@oregonstate.edu>
2017-10-17 22:26:23 -0500

    es6 syntax used in async callbacks

--- react-server-connection-test/frontend/src/App.js
+++ react-server-connection-test/frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 import Login from './Login';
 import NormalHome from './NormalHome';
@@ -7,23 +7,21 @@ import AdminHome from './AdminHome';
 
 class App extends Component {
   constructor(props) {
-      super(props);
+    super(props);
 
-      this.loggedIn = this.loggedIn.bind(this);
-      this.logIn = this.logIn.bind(this);
-      this.logout = this.logout.bind(this);
+    this.checkIfLoggedIn = this.checkIfLoggedIn.bind(this);
+    this.logIn = this.logIn.bind(this);
+    this.logout = this.logout.bind(this);
 
-      this.state = {
-        loading: true,
-        loggedIn: false,
-        userType: null
-      };
+    this.state = {
+      loading: true
+    };
 
-      this.loggedIn();
+    this.checkIfLoggedIn();
   }
 
-  loggedIn() {
-    axios.get('/auth').then(function (response) {
+  checkIfLoggedIn() {
+    axios.get('/auth').then((response) => {
       if(response.data.loggedIn) {
         this.setState({
           loggedIn: true,
@@ -31,52 +29,46 @@ class App extends Component {
         });
       }
       this.setState({ loading: false});
-    }.bind(this))
+    })
   }
 
   logIn(event) {
     event.preventDefault();
     const username = event.target.username.value;
     const password = event.target.password.value;
-    axios.post('/auth', {
-          username,
-          password
-        })
-        .then(function (response) {
-      if(response.data.loggedIn) {
-        this.setState({
-          loggedIn: true,
-          userType: response.data.userType,
-          errors: null
-        });
-      } else {
-        this.setState({
-          errors: response.data.errors
-        });
-      }
-    }.bind(this))
+    axios.post('/auth', { username, password })
+      .then((response) => {
+        if(response.data.loggedIn) {
+          this.setState({
+            loggedIn: true,
+            userType: response.data.userType,
+            errors: null
+          });
+        } else {
+          this.setState({
+            errors: response.data.errors
+          });
+        }
+    })
   }
 
   logout() {
-    axios.get('/logout').then(function (response) {
+    axios.get('/logout').then((response) => {
       this.setState({
         loggedIn: false,
         userType: null
       });
-    }.bind(this))
+    })
   }
 
   render() {
     if(this.state.loading) {
       return <div><h1>LOADING...</h1></div>;
-    }
-    else if(this.state.loggedIn && this.state.userType === "admin") {
-      return <AdminHome logout={this.logout}/>;
-    }
-    else if(this.state.loggedIn && this.state.userType === "normal") {
-      return <NormalHome />;
-    }
-    else {
+    } else if(this.state.loggedIn && this.state.userType === "admin") {
+      return <AdminHome logout={this.logout} />;
+    } else if(this.state.loggedIn && this.state.userType === "normal") {
+      return <NormalHome logout={this.logout} />;
+    } else {
       return <Login logIn={this.logIn} errors={this.state.errors}/>;
     }
   }
