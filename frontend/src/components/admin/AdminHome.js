1ee329bdb04d322d8bd1897295203b21a826981b
Matt Fry <fryma@oregonstate.edu>
2017-10-18 23:11:56 -0500

    merge admin home

--- frontend/src/components/admin/AdminHome.js
+++ frontend/src/components/admin/AdminHome.js
@@ -1,17 +1,93 @@
 import React, { Component } from 'react';
 
+import BussinessIntelligence from './BussinessIntelligence';
+import ManageUserAccounts from './ManageUserAccounts';
+import ManageAdminAccounts from './ManageAdminAccounts';
+
 class AdminHome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      currentPage: 'BussinessIntelligence' // home, edit account, send award
+    });
+
+    this.changePage = this.changePage.bind(this);
+    this.renderCurrentPageContent = this.renderCurrentPageContent.bind(this);
+  }
+
+  changePage(event) {
+    const newPage = event.target.name;
+    this.setState({
+      currentPage: newPage
+    });
+  }
+
+  renderCurrentPageContent() {
+    switch(this.state.currentPage) {
+      case 'BussinessIntelligence':
+        return <BussinessIntelligence />;
+      case 'ManageUserAccounts':
+        return <ManageUserAccounts />;
+      case 'ManageAdminAccounts':
+        return <ManageAdminAccounts />;
+      default:
+        return <div></div>;
+    }
+  }
+
+  createNavButtons() {
+    const tabs = [
+      {
+      text: 'Business Intelligence',
+      name: 'BussinessIntelligence'
+      },
+      {
+        text: 'Manage User Accounts',
+        name: 'ManageUserAccounts'
+      },
+      {
+        text: 'Manage Admin Accounts',
+        name: 'ManageAdminAccounts'
+      }
+    ];
+
+    return tabs.map(tab => {
+      let className = "nav-link";
+      if (this.state.currentPage === tab.name) {
+        className += " active";
+      }
+      return (
+        <li className="nav-item" key={tab.name}>
+          <a className={className}
+            name={tab.name}
+            onClick={this.changePage}
+          >{tab.text}</a>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
-      <div>
-        <h1>Admin Home</h1>
-        <div><button>Manage User Accounts</button></div>
-        <div><button>Manage Admin Accounts</button></div>
-        <div><button>Business Intelligence</button></div>
-        <div><button onClick={this.props.logOut}>Log Out</button></div>
+      <div className="col-md-10 col-md-offset-1">
+        <div className="row">
+          <div className="col-md-11"><h1>Admin Home</h1></div>
+          <div className="col-md-1 pull-right">
+            <button className="btn btn-primary" onClick={this.props.logOut}>Log Out</button>
+          </div>
+        </div>
+
+        <ul className="nav nav-tabs">
+          {this.createNavButtons()}
+        </ul>
+        <div className="userContent">
+          {this.renderCurrentPageContent()}
+        </div>
+
       </div>
     );
   }
+
 }
 
 export default AdminHome;
