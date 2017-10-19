775648faab45bdacf698fa8b7ed3ae16b96d3c9f
Matt Fry <fryma@oregonstate.edu>
2017-10-18 22:29:04 -0500

    Merge user home

--- react-server-connection-test/frontend/src/components/admin/AdminHome.js
+++ react-server-connection-test/frontend/src/components/admin/AdminHome.js
@@ -1,11 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class AdminHome extends Component {
   render() {
     return (
       <div>
-        <h1>Admin</h1>
-        <button onClick={this.props.logOut}>Log Out</button>
+        <h1>Admin Home</h1>
+        <div><button>Manage User Accounts</button></div>
+        <div><button>Manage Admin Accounts</button></div>
+        <div><button>Business Intelligence</button></div>
+        <div><button onClick={this.props.logOut}>Log Out</button></div>
       </div>
     );
   }
