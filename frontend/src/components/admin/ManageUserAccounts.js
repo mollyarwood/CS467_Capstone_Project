1ff3c8d911668a3267db64f81e35a0f003af78f2
Matt Fry <fryma@oregonstate.edu>
2017-10-18 23:33:45 -0500

    basic add user and edit user pages

--- frontend/src/components/admin/ManageUserAccounts.js
+++ frontend/src/components/admin/ManageUserAccounts.js
@@ -4,6 +4,7 @@ class ManageUserAccounts extends Component {
   constructor(props) {
       super(props);
       this.state = {
+        currentPage: 'view',
         users: [
           {
             id: 1,
@@ -17,6 +18,7 @@ class ManageUserAccounts extends Component {
       }
 
       this.deleteUser = this.deleteUser.bind(this);
+      this.changePage = this.changePage.bind(this);
   }
 
   deleteUser(event) {
@@ -28,42 +30,65 @@ class ManageUserAccounts extends Component {
     });
   }
 
+  changePage(event) {
+    const newPage = event.target.name;
+    this.setState({
+      currentPage: newPage
+    });
+  }
+
   render() {
-    return (
-      <div>
+    if (this.state.currentPage === 'add') {
+      return (
         <div>
-          <button className="btn btn-primary" onClick={this.deleteUser}>
-            Add User
-          </button>
+          <h3>Add user</h3>
+          <button className="btn" name='view' onClick={this.changePage}>Cancel</button>
         </div>
-        <table className="table">
-          <thead>
-            <tr className="row">
-              <th className="col-md-4">Username</th>
-              <th className="col-md-4">Name</th>
-              <th className="col-md-4">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.users.map( user =>
-              <tr className="row" key={user.id}>
-                <td className="col-md-4">{user.name}</td>
-                <td className="col-md-4">{user.name}</td>
-                <td className="col-md-2">
-                  <button id={user.id} className="btn btn-danger" onClick={this.deleteUser}>
-                    Edit
-                  </button>
-                </td>
-                <td className="col-md-2">
-                  <button id={user.id} className="btn btn-danger" onClick={this.deleteUser}>
-                    Delete
-                  </button>
-                </td>
-              </tr>)}
-          </tbody>
-        </table>
-      </div>
-    );
+      );
+    } else if (this.state.currentPage === 'edit') {
+      return (
+        <div>
+          <h3>Edit user</h3>
+          <button className="btn" name='view' onClick={this.changePage}>Cancel</button>
+        </div>
+      );
+    } else if (this.state.currentPage === 'view') {
+      return (
+        <div>
+          <div>
+            <button className="btn btn-primary" name="add" onClick={this.changePage}>
+              Add User
+            </button>
+          </div>
+          <table className="table">
+            <thead>
+              <tr className="row">
+                <th className="col-md-4">Username</th>
+                <th className="col-md-4">Name</th>
+                <th className="col-md-4">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.users.map( user =>
+                <tr className="row" key={user.id}>
+                  <td className="col-md-4">{user.name}</td>
+                  <td className="col-md-4">{user.name}</td>
+                  <td className="col-md-2">
+                    <button id={user.id} className="btn btn-danger" name="edit" onClick={this.changePage}>
+                      Edit
+                    </button>
+                  </td>
+                  <td className="col-md-2">
+                    <button id={user.id} className="btn btn-danger" onClick={this.deleteUser}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>)}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
   }
 }
 
