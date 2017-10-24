7c65a66777c40f810fac3c20682faa22c781093a
Matt Fry <fryma@oregonstate.edu>
2017-10-23 19:05:07 -0500

    Fix merge conflict

--- test_sessions/create_entities.py
+++ test_sessions/create_entities.py
@@ -1,28 +1,27 @@
+from google.appengine.ext import ndb
+import webapp2
+import urllib
+import json
+import datetime
+
+allowed_methods = webapp2.WSGIApplication.allowed_methods
+new_allowed_methods = allowed_methods.union(('PATCH',))
+webapp2.WSGIApplication.allowed_methods = new_allowed_methods
+
 # These are the two entities to be used in the Employee Award
 # system. I was unsure to include region or not, and as to how to
 # record that. GAE has a geographical point datatype, which contains
 # the latitude/longitude point. We would probably have to request the
 # user permission to access their current location.
-import os
-import webapp2
-import json
-import session_handler
-import datetime
-from time import mktime
-from webapp2_extras import sessions
-from google.appengine.api import app_identity
-from google.appengine.ext import ndb
-
-
 
 class Account(ndb.Model):
 	id = ndb.StringProperty()
 	username = ndb.StringProperty(required=True)
 	password = ndb.StringProperty(required=True)
 	name = ndb.StringProperty()
-	account_type = ndb.StringProperty(required=True)
-	creation_date = ndb.DateTimeProperty(auto_now_add=True )
-	last_modified = ndb.DateTimeProperty(auto_now=True)
+	account_type = ndb.StringProperty()
+	creation_date = ndb.DateTimeProperty()
+	last_modified = ndb.DateTimeProperty()
 	signature = ndb.BlobProperty()
 
 
@@ -32,77 +31,29 @@ class Award(ndb.Model):
 	recipient_email = ndb.StringProperty(required=True)
 	recipient_name = ndb.StringProperty()
 	award_type = ndb.StringProperty()
-	date_sent = ndb.DateTimeProperty(auto_now_add=True)
-	# region = ndb.GeoPtProperty()
-
-
-#extending the json class to handle datetime
-class MyEncoder(json.JSONEncoder):
-
-    def default(self, obj):
-        if isinstance(obj, datetime.datetime):
-            return int(mktime(obj.timetuple()))
-
-        return json.JSONEncoder.default(self, obj)
-
-
-class AccountCollectionHandler(session_handler.BaseHandler):
-
-	def get(self):
-		query = Account.query()
-		for account in query:
-			account = account.to_dict()
-			self.response.write(json.dumps(account, cls=MyEncoder))
-			self.response.write('\n')
-
-
-
-class AwardCollectionHandler(session_handler.BaseHandler):
-
-	def get(self):
-		query = Award.query()
-		for award in query:
-			award = award.to_dict()
-			self.response.write(json.dumps(award, cls=MyEncoder))
-			self.response.write('\n')
-
+	date_sent = ndb.DateTimeProperty()
 
 
 # This is the AccountHandler. It handles the post, patch, get
 # and delete functions. It returns the JSON of the entity upon
 # success, or an error message if the ID is not found.
-class AccountHandler(session_handler.BaseHandler):
+class AccountHandler():
+	def post(self):
 
-	def post(self, body=None):
+		account_data = json.loads(self.request.body)
 
-		if body:
-			account_data = body
-		else:
-			account_data = json.loads(self.request.body)
+		new_account = Account(username=account_data["username"],password=account_data["password"],creation_date=datetime.datetime.now(),last_modified=datetime.datetime.now())
 
-		username_already_exists = Account.query(Account.username == account_data['username']).get()
+		if 'name' in account_data:
+			new_account.name = account_data['name']
+		if 'account_type' in account_data:
+			new_account.account_type =account_data['account_type']
+		new_account.put()
+		new_account.id = new_account.key.urlsafe()
+		new_account.put()
 
-		# USERNAMES MUST BE UNIQUE ACROSS ACCOUNTS
-		if username_already_exists:
-			return False
-		else:
-			new_account = Account(username=account_data["username"],
-			password=account_data["password"],
-			account_type=account_data['account_type'])
-
-			if 'name' in account_data:
-				new_account.name = account_data['name']
-
-			new_account.put()
-			new_account.id = new_account.key.urlsafe()
-			new_account.put()
-			account_dict = new_account.to_dict()
-
-			# self.response.write(json.dumps(account_dict, cls=MyEncoder))
-			return True
-		#return json.dumps(account_dict, cls=MyEncoder)
-
-
+		# self.response.write(json.dumps(account.to_dict()))
+		return json.dumps(account.to_dict())
 
 	def patch(self, id=None):
 		if id:
@@ -121,13 +72,14 @@ class AccountHandler(session_handler.BaseHandler):
 				if 'signature' in account_data:
 					account.signature = account_data['signature']
 
+				account.last_modified = datetime.datetime.now()
+
 				account.put()
-				return json.dumps(account.to_dict(), cls=MyEncoder)
+				return json.dumps(account.to_dict())
 				# self.response.write(json.dumps(account.to_dict()))
 			else:
 				return "Error: accound ID not found"
 				# self.response.write("account ID not found")
-
 
 
 	def get(self, id=None):
@@ -139,8 +91,6 @@ class AccountHandler(session_handler.BaseHandler):
 			else:
 				return "Error: accound ID not found"
 				#self.response.write("account ID not found")
-
-
 
 	def delete(self, id=None):
 		if id:
@@ -154,32 +104,22 @@ class AccountHandler(session_handler.BaseHandler):
 				# self.response.write("account ID not found")
 
 
-
-
 # This is the AwardHandler. It handles the post, get
 # and delete functions. It returns the JSON of the entity upon
 # success, or an error message if the ID is not found.
-class AwardHandler(session_handler.BaseHandler):
+class AwardHandler():
+	def post(self):
 
-	def post(self, body):
+		award_data = json.loads(self.request.body)
 
-		if body:
-			award_data = body
-		else:
-			award_data = json.loads(self.request.body)
-
-		new_award = Award( 
-		sender=award_data["sender"],
-		recipient_name=award_data["recipient_name"],
-		recipient_email=award_data["recipient_email"],
-		award_type=award_data["award_type"])
+		new_award = Award(sender=award_data["sender"], recipient_name=award_data["recipient_name"], recipient_email=award_data["recipient_email"], date_sent=datetime.datetime.now(), award_type=award_data["award_type"])
 
 		new_award.put()
 		new_award.id = new_award.key.urlsafe()
 		new_award.put()
 
 		# self.response.write(json.dumps(award.to_dict()))
-		return True
+		return json.dumps(award.to_dict())
 
 	# User should not be able to edit an award once its sent.
 
@@ -206,8 +146,6 @@ class AwardHandler(session_handler.BaseHandler):
 			else:
 				return "Error: accound ID not found"
 				#self.response.write("award ID not found")
-
-
 
 	def delete(self, id=None):
 		if id:
